b7e3a38352b7fda6ec0cccc3b55b84c530b6bd12
Million Bereket <million.bereket16@gmail.com>
2025-05-29 14:54:08 -0500

    Updated my Sandwich class for Capstone-2

--- DELI-iousREMAKE/src/main/java/Sandwich.java
+++ DELI-iousREMAKE/src/main/java/Sandwich.java
@@ -18,10 +18,38 @@ public class Sandwich {
         this.breadType = breadType;
         this.isToasted = isToasted;
 
-
         this.meats = new ArrayList<>();
         this.cheeses = new ArrayList<>();
         this.toppings = new ArrayList<>();
         this.sauces = new ArrayList<>();
+    }
+
+    public void addMeat(String meat) {
+        meats.add(meat);
+    }
+
+    public void addCheese(String cheese) {
+        cheeses.add(cheese);
+    }
+
+    public void addTopping(String topping) {
+        toppings.add(topping);
+    }
+
+    public void addSauce(String sauce) {
+        sauces.add(sauce);
+    }
+
+    public double calculatePrice() {
+        double price = 0.0;
+
+        if (size.equals("4")) {
+            price = 5.50;
+        } else if (size.equals("8")) {
+            price = 7.00;
+        } else if (size.equals("12")) {
+            price = 8.50;
+        }
+
     }
 }
