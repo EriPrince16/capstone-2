308bebab85498530d8b2980f5009c9113945570c
Million Bereket <million.bereket16@gmail.com>
2025-05-30 02:30:08 -0500

    Created methods for Sandwich , Drink and Chips for Main class (Capstone-2).

--- DELI-iousREMAKE/src/main/java/Main.java
+++ DELI-iousREMAKE/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         boolean keepGoing = true;
 
         while (keepGoing) {
@@ -40,5 +42,76 @@ public class Main {
 
         }
         scanner.close();
+    }
+
+    // Build a sandwich by asking the user questions
+    public static Sandwich createSandwich() {
+        System.out.print("Choose bread (white, wheat, rye, wrap): ");
+        String bread = scanner.nextLine();
+
+        System.out.print("Choose size (4, 8, 12): ");
+        String size = scanner.nextLine();
+
+        System.out.print("Do you want it toasted? (yes or no): ");
+        boolean toasted = scanner.nextLine().equalsIgnoreCase("yes");
+
+        Sandwich sandwich = new Sandwich(size, bread, toasted);
+
+        // Add meats
+        System.out.println("Add meats (type 'done' to stop): ");
+        while (true) {
+            System.out.print("Meat: ");
+            String meat = scanner.nextLine();
+            if (meat.equalsIgnoreCase("done")) break;
+            sandwich.addMeat(meat);
+        }
+
+        // Add cheeses
+        System.out.println("Add cheeses (type 'done' to stop): ");
+        while (true) {
+            System.out.print("Cheese: ");
+            String cheese = scanner.nextLine();
+            if (cheese.equalsIgnoreCase("done")) break;
+            sandwich.addCheese(cheese);
+        }
+
+        // Add regular toppings
+        System.out.println("Add toppings (type 'done' to stop): ");
+        while (true) {
+            System.out.print("Topping: ");
+            String topping = scanner.nextLine();
+            if (topping.equalsIgnoreCase("done")) break;
+            sandwich.addTopping(topping);
+        }
+
+        // Add sauces
+        System.out.println("Add sauces (type 'done' to stop): ");
+        while (true) {
+            System.out.print("Sauce: ");
+            String sauce = scanner.nextLine();
+            if (sauce.equalsIgnoreCase("done")) break;
+            sandwich.addSauce(sauce);
+        }
+
+        return sandwich;
+    }
+
+    // Build a drink
+    public static Drink createDrink() {
+        System.out.print("Choose drink size (small, medium, large): ");
+        String size = scanner.nextLine();
+
+        System.out.print("Choose drink flavor: ");
+        String flavor = scanner.nextLine();
+
+        return new Drink(size, flavor);
+    }
+
+    // Build a chips
+    public static Chips createChips() {
+        System.out.print("Choose chips type (BBQ, sour cream, etc.): ");
+        String type = scanner.nextLine();
+
+        return new Chips(type);
     }
 }
